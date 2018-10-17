2272cf6c5f5d38d9fa30546379d10908766c7181
Mateusz Tokarski <mati.tokarski@gmail.com>
2018-10-17 10:46:17 +0300

    [Feature] Move price calculation to backend

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,11 +1,34 @@
-import { map } from "lodash";
+import { curry, reduce, map, get } from "lodash";
 import shortid from "shortid";
 
-const getLatestCurrencyRates = async currency => await currency.latest();
+const calculateBookingDatePrice = curry(
+  async (prisma, personCount, totalPricePromise, date) => {
+    const totalPrice = await totalPricePromise;
+    const activityName = get(date, "activity", null);
+    if (activityName === null) {
+      return await totalPrice;
+    }
 
-const createBookingPayload = data => ({
+    const activity = await prisma.activity({ name: activityName });
+    if (activity === null) {
+      throw new Error("Activity not found");
+    }
+
+    return totalPrice + get(activity, "pricePerPerson");
+  }
+);
+
+const calculateBookingPrice = async (prisma, { personCount, dates }) =>
+  reduce(
+    dates,
+    calculateBookingDatePrice(prisma, personCount),
+    Promise.resolve(0)
+  );
+
+const createBookingPayload = async (prisma, data) => ({
   ...data,
   number: shortid.generate(),
+  priceTotal: await calculateBookingPrice(prisma, data),
   dates: {
     create: map(data.dates, bookingDate => ({
       date: bookingDate.date,
@@ -22,9 +45,13 @@ const upsertCurrencyPayload = (rate, code, date) => ({
   create: { code, rate, date }
 });
 
+const getLatestCurrencyRates = async currency => await currency.latest();
+
 const Mutation = {
-  createBooking: (parent, { data }, { prisma }, info) =>
-    prisma.createBooking(createBookingPayload(data), info),
+  calculateBookingPrice: async (parent, { data }, { prisma }) =>
+    calculateBookingPrice(prisma, data),
+  createBooking: async (parent, { data }, { prisma }, info) =>
+    prisma.createBooking(await createBookingPayload(data), info),
   updateCurrencyRates: async (parent, args, { prisma, currency }) => {
     const latest = await getLatestCurrencyRates(currency);
     const { date, rates } = latest;
